a5fdba772ffbaea0cde493d176ad139124939bd0
jitensya <knjmtm71+git@gmail.com>
2025-05-05 02:33:53 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -9,18 +9,27 @@
 <body>
    <p>
        <?php
-       //数値の配列
-       $numbers = [80,60,55,40,100,25,80,95,30,60];
+
+       $score1 = 80;
+       $score1 = 60;
+       $score1 = 55;
+       $score1 = 40;
+       $score1 = 100;
+       $score1 = 25;
+       $score1 = 80;
+       $score1 = 95;
+       $score1 = 30;
+       $score1 = 60;
+
+       echo $result1 = ($score1+$score2+$score3+$score4+$score5+$score6+$score7+$score8+$score9+$score10)/10;
+
+
+
       
-       //合計を計算
-       $sum = array_sum($numbers);
-
-       //平均を計算
-       $average = $sum / count($numbers);
-
-       //結果を表示
-       //echo "合計: ,$sum" ;
-       echo "平均: ,$average";
+       //$numbers = [80,60,55,40,100,25,80,95,30,60];
+       //$sum = array_sum($numbers);
+       //$average = $sum / count($numbers);
+       //echo "平均: ,$average";
        ?>
    </p>
 </body>
